cbb7bb9b74633c04203804c5b51a8624f284c9f2
felix-nexus <felix@to.nexus>
2025-07-22 17:58:23 +0900

    chore go bind

--- binding/go/src/ForgeV1.go
+++ binding/go/src/ForgeV1.go
@@ -148,6 +148,18 @@ func (forgeV1 *ForgeV1) PackEip712Domain() []byte {
 	return enc
 }
 
+// Eip712DomainOutput serves as a container for the return parameters of contract
+// method Eip712Domain.
+type Eip712DomainOutput struct {
+	Fields            [1]byte
+	Name              string
+	Version           string
+	ChainId           *big.Int
+	VerifyingContract common.Address
+	Salt              [32]byte
+	Extensions        []*big.Int
+}
+
 // UnpackEip712Domain is the Go binding that unpacks the parameters returned
 // from invoking the contract method with ID 0x84b0196e.
 //
